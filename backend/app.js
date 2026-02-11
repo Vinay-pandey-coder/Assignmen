529fe5caf32f1d262ef8cf5258cfc93618af02a4
vinaypandey <abhi837688@gmail.com>
2026-02-11 08:10:12 +0530

    Assignmen

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth.routes");
-const taskRoutes = require("./routes/task.routes");
-const userRoutes = require("./routes/user.routes");
+// Corrected paths
+const authRoutes = require("./src/routes/auth.routes");
+const taskRoutes = require("./src/routes/task.routes");
+const userRoutes = require("./src/routes/user.routes");
 
-const errorHandler = require("./middlewares/error.middleware");
+const errorHandler = require("./src/middlewares/error.middleware");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes);
 
+// Error handler
 app.use(errorHandler);
 
 module.exports = app;
