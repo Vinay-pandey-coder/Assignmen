885b8f3fe4649acb942823aa2c1fb76454288e65
vinaypandey <abhi837688@gmail.com>
2026-02-11 13:25:31 +0530

    Assignmen

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://assignmen-backend.vercel.app/",
+  baseURL: "https://assignmen-backend.vercel.app/api",
 });
 
 // Attach token to every request
